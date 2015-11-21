d615b935b6c04911112d017f57712a39b2c68e71
unknown <katva02@KATVA02.ca.com>
2015-11-21 00:31:33 -0600

    'changes'

--- LoginService/src/main/java/com/osrs/login/controller/LoginController.java
+++ LoginService/src/main/java/com/osrs/login/controller/LoginController.java
@@ -3,6 +3,8 @@ package com.osrs.login.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.osrs.login.service.LoginService;
 
@@ -12,10 +14,9 @@ import com.osrs.login.service.LoginService;
  * has methods like login,validateSession etc. TODO: Expand the description once all information is available
  */
 @Controller
+@RequestMapping("/login")
 public class LoginController {
 	
-	@Autowired
-	LoginService loginService;
 	
 	/**
 	 * @param userName
@@ -24,9 +25,10 @@ public class LoginController {
 	 * This method tries to login to the application and return a 
 	 * sessionKey for the user.
  	 */
-	@RequestMapping("/login")
-	public String login(String userName,String password){
-		return loginService.login(userName, password);
+	@RequestMapping(method = RequestMethod.GET)
+	public @ResponseBody String login(){
+		System.out.println("Reached login");
+		return "sessionKey";
 	}
 	
 	/**
@@ -34,7 +36,7 @@ public class LoginController {
 	 * @return sessionKey
 	 * Auto login the user based on a key.
 	 */
-	@RequestMapping
+	@RequestMapping("/autologin")
 	public String autoLogin(String key){
 		
 		return "";
